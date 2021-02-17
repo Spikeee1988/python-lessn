cebbeaa0ba6181b3139b62e88530b7402ad602b7
Ваше Имя <you@example.com>
2021-02-17 22:33:28 +0300

    added home work

---
+++ homework 2.py
@@ -0,0 +1,38 @@
+# задание 1
+a = 10
+b = 5
+sum = (a + b)
+raz = (a - b)
+mult = (a * b)
+print(sum)
+print(raz)
+print(mult)
+# задание 2
+x = 10
+y = 5
+print((x - y) / (1 + x * y))
+# задание 3
+d = 5
+v = d ** 3
+s = d * d
+print(v)
+print(s)
+# задание 4
+a = 7
+b = 3
+sum = (a + b)
+# среднее арифм.
+print(sum / 2)
+# среднее геометр.
+geo = a * b
+print(geo ** 2)
+# задание 5
+a = 7
+b = 3
+# гипотенуза
+from math import hypot
+c = hypot(a , b)
+print(c)
+st = (a * b / 2)
+# площадь треугольника
+print(st)
